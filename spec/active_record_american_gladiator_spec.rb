e429714f6121a6da34aaf16f4437bde37bab5698
Hugh Morrison <hmorri32@gmail.com>
2017-11-18 20:08:23 -0700

    altaslphere

--- spec/active_record_american_gladiator_spec.rb
+++ spec/active_record_american_gladiator_spec.rb
@@ -19,37 +19,30 @@ describe "ActiveRecord American Gladiator" do
       Item.create(name: "Powerball Goal")
       Item.create(name: "Trap Door")
 
-      items = Item.where(:name).where('name = ?', 'Powerball')
-
+      items = Item.where('name LIKE ?', '%Powerball%')
 
       expect(items.count).to eq(2)
     end
   end
 
   context "Hang Tough" do
-    xit "returns orders for 3 users in 2 queries (aka: Remove the N+1 query)" do
+    it "returns orders for 3 users in 2 queries (aka: Remove the N+1 query)" do
       diamond  = User.create(name: "Diamond")
       turbo    = User.create(name: "Turbo")
       laser    = User.create(name: "Laser")
       ice      = User.create(name: "Ice")
-
-      order_1 = diamond.orders.create(amount: 1)
-      order_2 = turbo.orders.create(amount: 2)
-      order_3 = ice.orders.create(amount: 3)
-      order_4 = laser.orders.create(amount: 4)
-
+      order_1  = diamond.orders.create(amount: 1)
+      order_2  = turbo.orders.create(amount: 2)
+      order_3  = ice.orders.create(amount: 3)
+      order_4  = laser.orders.create(amount: 4)
 
       order_amounts = []
 
-      # Changeable Start
-      users = User.first(3)
-      # Changeable End
+      users = User.includes(:orders).first(3)
 
-      # Use eager loading to remove the N+1 query
       # Hint: Read until section 13.1 - http://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations
 
       users.each do |user|
-        # The problem: Each time we call user.orders, we go to the DB to pull them down.
         order_amounts << user.orders.first.amount
       end
 
@@ -59,7 +52,7 @@ describe "ActiveRecord American Gladiator" do
   end
 
   context "The Maze" do
-    xit "returns all users that have placed an order" do
+    it "returns all users that have placed an order" do
       gemini = User.create(name: "Gemini")
       sky    = User.create(name: "Sky")
       nitro  = User.create(name: "Nitro")
@@ -68,20 +61,16 @@ describe "ActiveRecord American Gladiator" do
       sky.orders.create
       nitro.orders.create
 
-      # Changeable Start
-      active_users = User.all.select do |user|
-        user.orders.present?
-      end
-      # Changeable End
+      active_users = User.joins(:orders).uniq
 
+      # active_users = User.joins(:orders).distinct
       # Hint: http://guides.rubyonrails.org/active_record_querying.html#joining-tables
-
       expect(active_users).to eq([sky, nitro])
     end
   end
 
   context "Breakthrough and Conquer" do
-    xit "returns all orders with Footballs and Wrestling Rings" do
+    it "returns all orders with Footballs and Wrestling Rings" do
       wrestling_ring = Item.create(name: "Wrestling Ring")
       football       = Item.create(name: "Football")
       sweat          = Item.create(name: "Sweat")
@@ -89,11 +78,9 @@ describe "ActiveRecord American Gladiator" do
       order_2        = Order.create(items: [sweat])
       order_3        = Order.create(items: [football])
 
-      # Changeable Start
-      orders = Order.all.select do |order|
-        order.items.include?(football) || order.items.include?(wrestling_ring)
-      end
-      # Changeable End
+      orders = Order.joins(:order_items).where(order_items: {item: [wrestling_ring, football]})
+
+      # User.joins(:posts).where({ posts: { published: true } })
 
       # Hint: Take a look at the `Joins` section and the example that combines `joins` and `where` here: http://apidock.com/rails/ActiveRecord/QueryMethods/where
 
@@ -102,8 +89,8 @@ describe "ActiveRecord American Gladiator" do
   end
 
   context "The Eliminator" do
-    xit "returns all orders placed 2 weeks ago" do
-      last_week = Date.today.last_week
+    it "returns all orders placed 2 weeks ago" do
+      last_week     = Date.today.last_week
       two_weeks_ago = Date.today.last_week - 7.days
 
       order_1 = Order.create(created_at: two_weeks_ago)
@@ -112,18 +99,17 @@ describe "ActiveRecord American Gladiator" do
       order_4 = Order.create(created_at: last_week + 1.day)
       order_5 = Order.create(created_at: two_weeks_ago + 2.days)
 
-      # Changeable Start
-      orders = Order.all.select do |order|
-        order.created_at >= two_weeks_ago && order.created_at <= last_week
-      end
-      # Changeable End
+      # orders = Order.all.select do |order|
+      #   order.created_at >= two_weeks_ago && order.created_at <= last_week
+      # end
+
+      orders = Order.where(created_at: [two_weeks_ago..last_week])
 
       expect(orders).to eq([order_1, order_3, order_5])
     end
   end
 
   context "Atlasphere" do
-    # This one is challenging.
     xit "returns most popular items" do
       scoring_pod = Item.create(name: "Scoring Pod")
       lights      = Item.create(name: "Lights")
@@ -133,7 +119,7 @@ describe "ActiveRecord American Gladiator" do
       Order.create(items: [lights, smoke, smoke])
       Order.create(items: [lights, lights, lights])
 
-      # Changeable Start
+      # Start
       items_with_count = Hash.new(0)
 
       Order.all.each do |order|
@@ -151,7 +137,7 @@ describe "ActiveRecord American Gladiator" do
       most_popular_items = top_item_ids.map do |id|
         Item.find(id)
       end
-      # Changeable Stop
+
 
       # Hints: http://apidock.com/rails/ActiveRecord/QueryMethods/select
       #        http://stackoverflow.com/questions/8696005/rails-3-activerecord-order-by-count-on-association
